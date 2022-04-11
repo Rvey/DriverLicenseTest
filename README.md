b69ab289aacbf413f0a51b421b90ec830bcde11f
Redouane Bouabana <rredouane342@gmail.com>
2022-04-11 23:17:57 +0000

    init

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# DriverLicenseTest
- 
+# vite-react-electron
---
+++ packages/renderer/src/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './styles/index.css'
+
+const root = createRoot(document.getElementById('root')!)
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
+
+window.removeLoading()
+
+console.log('fs', window.fs)
+console.log('ipcRenderer', window.ipcRenderer)
+
+// Usage of ipcRenderer.on
+window.ipcRenderer.on('main-process-message', (_event, ...args) => {
+  console.log('[Receive Main-process message]:', ...args)
+})
