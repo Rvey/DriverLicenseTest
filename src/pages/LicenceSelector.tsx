1d246c76c72c5df3ff28ed209091db0bf45713ff
Redouane Bouabana <rredouane342@gmail.com>
2022-04-14 10:24:23 +0000

    🚀

--- src/pages/LicenceSelector.tsx
+++ src/pages/LicenceSelector.tsx
@@ -6,7 +6,7 @@ const LicenceType: React.VFC = () => {
 
     return (
         <div className="flex justify-center flex-col h-screen bg-cover bg-[url(https://wallpaperaccess.com/full/6712593.jpg)]">
-
+            <h1 className="text-center text-gray-700 text-4xl font-bold mb-[5em] bg-gray-50 py-4 bg-opacity-60">My Permis</h1>
             <div className="flex justify-center items-center" >
                 <LicenceTypeCard licenceType="Permis de voiture" imgSrc="https://img.icons8.com/ios-glyphs/90/000000/car--v1.png" url="/carLicence"/>
                 <LicenceTypeCard licenceType="Permis de camion" imgSrc="https://img.icons8.com/ios-filled/100/000000/truck.png" url="/truckLicence"/>
